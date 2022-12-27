6a6903e353a3ec99134514937f972b3afdabe5ab
Gilvan-R-A <68397059+Gilvan-R-A@users.noreply.github.com>
2022-12-27 16:37:49 -0300

    Adicionando um convidado

--- gestao-festa/src/main/java/com/empresa/gestaofesta/controller/ConvidadosController.java
+++ gestao-festa/src/main/java/com/empresa/gestaofesta/controller/ConvidadosController.java
@@ -1,23 +1,36 @@
 package com.empresa.gestaofesta.controller;
 
+import com.empresa.gestaofesta.model.Convidado;
 import com.empresa.gestaofesta.repository.Convidados;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
     @Autowired
     private Convidados convidados;
 
-    @GetMapping("/convidados")
+    @GetMapping
     public ModelAndView listar(){
         ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 
         modelAndView.addObject("convidados", convidados.findAll());
-
         return modelAndView;
     }
+
+    @PostMapping
+    public String salvar(Convidado convidado){
+        this.convidados.save(convidado);
+        return "redirect:/convidados";
+    }
+
+
+
+
 }
